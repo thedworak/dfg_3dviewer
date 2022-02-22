847339d1ae11a024e2b824ebb109c29530b294bf
Daniel Dworak <thedworak@gmail.com>
2022-02-22 15:12:02 +0000

    Metadata settings changed

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ var guiContainer = document.createElement("div");
 guiContainer.id = 'guiContainer';
 guiContainer.className = 'guiContainer';
 guiContainer.style.position = 'absolute';
-guiContainer.style.left = '80%';
+guiContainer.style.right = '10%';
 guiContainer.style.marginTop = '0px';
 var guiElement = document.createElement("div");
 guiElement.id = 'guiContainer';
@@ -108,6 +108,10 @@ const colors = {
 }
 
 const intensity = { startIntensity: 1 }
+
+const saveProperties = {
+	Camera: true
+}
 
 var EDITOR = false;
 
@@ -156,6 +160,7 @@ var clippingPlanes = [
 		new THREE.Plane( new THREE.Vector3( 0, 0, - 1 ), 0 )
 	];
 var planeHelpers, clippingFolder;
+var propertiesFolder;
 var planeObjects = [];
 
 var clippingGeometry = [];
@@ -322,7 +327,7 @@ function init() {
 	{ 
 		if (value == '') transformControl.detach(); else { transformControl.mode = value; transformControl.attach( helperObjects[0] ); }
 	});
-	const lightFolder = editorFolder.addFolder('Lights');
+	const lightFolder = editorFolder.addFolder('Lights').close();
 	lightFolder.add(transformText, 'Transform Light', { None: '', Move: 'translate', Rotate: 'rotate', Scale: 'scale' } ).onChange(function (value)
 	{ 
 		if (value == '') transformControlLight.detach(); else { transformControlLight.mode = value; transformControlLight.attach( lightObjects[0] ); }
@@ -334,7 +339,9 @@ function init() {
 	lightFolder.add( intensity, 'startIntensity', 0, 10 ).onChange(function (value) {
 		lightObjects[0].intensity = value;
 	});
-	clippingFolder = editorFolder.addFolder('Clipping Planes');
+	clippingFolder = editorFolder.addFolder('Clipping Planes').close();
+	propertiesFolder = editorFolder.addFolder('Save properties').close();
+	propertiesFolder.add( saveProperties, 'Camera' ); 
 
 	if (editor)
 		editorFolder.add({["Save"]: function(){
@@ -348,7 +355,10 @@ function init() {
 			//console.log(camera.position);
 			var rotateMetadata = new THREE.Vector3(THREE.Math.radToDeg(helperObjects[0].rotation.x),THREE.Math.radToDeg(helperObjects[0].rotation.y),THREE.Math.radToDeg(helperObjects[0].rotation.z));
 			var newMetadata = ({"objPosition": [ helperObjects[0].position.x, helperObjects[0].position.y, helperObjects[0].position.z ], "objScale": [ helperObjects[0].scale.x, helperObjects[0].scale.y, helperObjects[0].scale.z ], "objRotation": [ rotateMetadata.x, rotateMetadata.y, rotateMetadata.z ], "camPosition": [ camera.position.x, camera.position.y, camera.position.z ], "camLookAt": [ 0, 0, 0 ], "lightPosition": [ dirLight.position.x, dirLight.position.y, dirLight.position.z ], "lightColor": [ "#" + (dirLight.color.getHexString()).toUpperCase() ], "lightIntensity": [ dirLight.intensity ] });
-			console.log(uri+basename+"/");
+			console.log(newMetadata);
+			if (saveProperties.Camera)
+				newMetadata = Object.assign(newMetadata, {"cameraPosition": [ camera.position.x, camera.position.y, camera.position.z ], "cameraTarget": [ controls.target.x, controls.target.y, controls.target.z ]});
+			//console.log(uri+basename+"/");
 			if (compressedFile != '')
 				var params = "5MJQTqB7W4uwBPUe="+JSON.stringify(newMetadata, null, '\t')+"&path="+uri+basename+compressedFile+"&filename="+filename;
 			else
@@ -554,7 +564,7 @@ function loadModel ( path, basename, filename, extension, org_extension ) {
 							child.geometry.computeVertexNormals();
 							if(child.material.map) child.material.map.anisotropy = 16;
 							child.material.side = THREE.DoubleSide;
-							for ( let i = 0; i < 3; i ++ ) {
+							/*for ( let i = 0; i < 3; i ++ ) {
 
 								const poGroup = new THREE.Group();
 								const plane = clippingPlanes[ i ];
@@ -579,9 +589,7 @@ function loadModel ( path, basename, filename, extension, org_extension ) {
 									} );
 								const po = new THREE.Mesh( planeGeom, planeMat );
 								po.onAfterRender = function ( renderer ) {
-
 									renderer.clearStencil();
-
 								};
 
 								po.renderOrder = i + 1.1;
@@ -591,7 +599,7 @@ function loadModel ( path, basename, filename, extension, org_extension ) {
 								planeObjects.push( po );
 								scene.add( poGroup );
 
-							}
+							}*/
 							child.material.clippingPlanes = clippingPlanes;
 							child.material.clipIntersection = false;
 							mainObject.push(child);	
@@ -799,13 +807,18 @@ function setupObject (_object, _camera, _light, _data, _controls) {
 
 function setupCamera (_object, _camera, _light, _data, _controls) {
 	if (typeof (_data) != "undefined") {
-		_camera.position.set( _data["camPosition"][0], _data["camPosition"][1], _data["camPosition"][2] );
-		_light.position.set( _data["lightPosition"][0], _data["lightPosition"][1], _data["lightPosition"][2] );
-		_light.color = new THREE.Color( _data["lightColor"][0] );
-		_light.intensity = _data["lightIntensity"][0];
+		if (typeof (_data["cameraPosition"]) != "undefined") {
+			_camera.position.set (_data["cameraPosition"][0], _data["cameraPosition"][1], _data["cameraPosition"][2]);
+			_controls.target.set (_data["cameraTarget"][0], _data["cameraTarget"][1], _data["cameraTarget"][2]);
+		}
+		if (typeof (_data["lightPosition"]) != "undefined") {
+			_light.position.set( _data["lightPosition"][0], _data["lightPosition"][1], _data["lightPosition"][2] );
+			_light.color = new THREE.Color( _data["lightColor"][0] );
+			_light.intensity = _data["lightIntensity"][0];
+		}
 		_camera.updateProjectionMatrix();
 		_controls.update();
-		fitCameraToCenteredObject ( _camera, _object, 1.7, _controls );
+		fitCameraToCenteredObject ( _camera, _object, 1.7, _controls, true );
 	}
 	else {
 		var boundingBox = new THREE.Box3();
@@ -820,7 +833,7 @@ function setupCamera (_object, _camera, _light, _data, _controls) {
 		var size = new THREE.Vector3();
 		boundingBox.getSize(size);
 		camera.position.set(size.x, size.y, size.z);
-		fitCameraToCenteredObject ( _camera, _object, 1.7, _controls );
+		fitCameraToCenteredObject ( _camera, _object, 1.7, _controls, false );
 	}
 }
 
@@ -877,7 +890,7 @@ function animate() {
 	stats.update();
 }
 
-function fitCameraToCenteredObject (camera, object, offset, orbitControls ) {
+function fitCameraToCenteredObject (camera, object, offset, orbitControls, _fit ) {
 	const boundingBox = new THREE.Box3();
 	if (Array.isArray(object)) {
 		for (var i = 0; i < object.length; i++) {			
@@ -952,6 +965,7 @@ function fitCameraToCenteredObject (camera, object, offset, orbitControls ) {
     let dx = size.z / 2 + Math.abs( size.x / 2 / Math.tan( fovh / 2 ) );
     let dy = size.z / 2 + Math.abs( size.y / 2 / Math.tan( fov / 2 ) );
     let cameraZ = Math.max(dx, dy);
+	if (_fit) cameraZ = camera.position.z;
 
     // offset the camera, if desired (to avoid filling the whole canvas)
     if( offset !== undefined && offset !== 0 ) cameraZ *= offset;
