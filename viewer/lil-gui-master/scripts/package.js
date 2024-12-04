01dbd888c66b8550af2491c0ffe1ea1ce018211b
TheDworak <thedworakstudios@gmail.com>
2024-12-04 11:00:35 +0100

    Removed lil-gui-master repo

--- viewer/lil-gui-master/scripts/package.js
+++
@@ -1,6 +0,0 @@
-// workaround to import json without warnings from node@18 and eslint
-// https://github.com/eslint/eslint/discussions/15305
-
-import fs from 'fs';
-
-export default JSON.parse( fs.readFileSync( './package.json' ) );
