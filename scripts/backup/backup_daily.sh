5f78372f0cd950effc4bb5974050862b714dc90e
Daniel Dworak <thedworak@gmail.com>
2024-12-18 11:20:29 +0100

    Changed paths

--- scripts/backup/backup_daily.sh
+++ scripts/backup/backup_daily.sh
@@ -9,6 +9,7 @@ error() {
     echo "Error: $1" && exit 1
 }
 
+REPONAME="3drepository"
 BPATH="/var/www/data/backups/daily"
 BDPATH="/var/www/data/backups/daily/db"
 WEBSITE="www.dfg-repository.wisski.cloud"
@@ -17,6 +18,7 @@ WEBSITE="www.dfg-repository.wisski.cloud"
 CURRENT_DATE=$(date +%d%m%y%H%M)
 TGZ_FILE="${BPATH}/backup_${WEBSITE}_${CURRENT_DATE}.tgz"
 SQL_FILE="${BDPATH}/${WEBSITE}_databases_${CURRENT_DATE}.sql"
+BSOURCE="/var/www/html/${REPONAME}"
 
 mkdir -p "$BPATH"
 
@@ -24,9 +26,9 @@ mkdir -p "$BPATH"
 mkdir -p "${BDPATH}"
 
 source read_settings.sh
-mysqldump -h ${DATABASE_HOST} -u ${DATABASE_USER} -p${DATABASE_PASS} ${DATABASE} --hex-blob --skip-lock-tables --single-transaction > "$SQL_FILE"
+#mysqldump -h ${DATABASE_HOST} -u ${DATABASE_USER} -p${DATABASE_PASS} ${DATABASE} --hex-blob --skip-lock-tables --single-transaction > "$SQL_FILE"
 
-tar --exclude="${BPATH:1}" --warning=none -czpf "$TGZ_FILE" -C / var/www/data
+tar --exclude="${BPATH:1}" --warning=none -czpf "${TGZ_FILE}" -C / "$BSOURCE" var/www/data
 
 #scp "$TGZ_FILE" "${USER}@example.work:/mnt/home/${USER}"
 
