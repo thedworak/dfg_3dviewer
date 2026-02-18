4aeeaeadb5f9ba874036e62c38ff7d51e5058dbf
thedworak <96061043+thedworak@users.noreply.github.com>
2026-02-18 15:35:43 +0100

    Service for saving created

--- src/Controller/ProgressController.php
+++ src/Controller/ProgressController.php
@@ -1,12 +1,22 @@
-public function check($entity_id) {
+<?php
 
-  $entity = \Drupal::entityTypeManager()
-    ->getStorage('wisski_individual')
-    ->load($entity_id);
+namespace Drupal\dfg_3dviewer\Controller;
 
-  return new JsonResponse([
-    'status' => $entity->get('field_processing_status')->value,
-    'progress' => (int)$entity->get('processing_progress')->value,
-    'message' => $entity->get('processing_message')->value,
-  ]);
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Controller\ControllerBase;
+
+class ProgressController extends ControllerBase {
+
+    public function check($entity_id) {
+
+    $entity = \Drupal::entityTypeManager()
+        ->getStorage('wisski_individual')
+        ->load($entity_id);
+
+    return new JsonResponse([
+        'status' => $entity->get('field_processing_status')->value,
+        'progress' => (int)$entity->get('processing_progress')->value,
+        'message' => $entity->get('processing_message')->value,
+    ]);
+    }
 }
