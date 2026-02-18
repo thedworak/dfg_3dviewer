f2336781b138a1d1aef718f75b66a4888555e990
thedworak <96061043+thedworak@users.noreply.github.com>
2026-02-18 11:06:10 +0100

    Added service for models

---
+++ src/ModelFormatManager.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\dfg_3dviewer;
+
+class ModelFormatManager {
+
+  protected array $allowedModelFormats = [
+    'abc', 'obj', 'fbx', 'ply', 'dae', 'ifc',
+    'stl', 'xyz', 'pcd', 'json', '3ds',
+    'blend', 'gml', 'wrl', 'glb', 'gltf'
+  ];
+
+  protected array $zipFormats = [
+    'zip', 'rar', 'tar', 'xz', 'gz'
+  ];
+
+  public function getAllowedModelFormats(): array {
+    return $this->allowedModelFormats;
+  }
+
+  public function getZipFormats(): array {
+    return $this->zipFormats;
+  }
+
+  public function getAllFormats(): array {
+    return array_merge($this->allowedModelFormats, $this->zipFormats);
+  }
+}
