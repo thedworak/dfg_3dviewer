249fb90c0cab59f29505d37081aa34e4eaf717fc
thedworak <96061043+thedworak@users.noreply.github.com>
2026-03-09 10:22:44 +0100

    Make scripts executable

--- scripts/render.sh
+++ scripts/render.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BLENDER_PATH=''
+BLENDER_PATH='blender' # Default to system blender, can be overridden by .env
 source "${SCRIPT_DIR}/.env"
 BLENDER_PATH="${SCRIPT_DIR}/${BLENDER_PATH}"
 
