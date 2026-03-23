aed363a3a59c8137c6562c6d5b4803d4dbcbcc76
Daniel <thedworak@gmail.com>
2026-03-23 09:51:44 +0100

    Drupal paths changes

--- settings.php
+++ settings.php
@@ -1,1 +1,2 @@
-$settings['dfg_env'] = 'drupal'; // possible values 'prod', 'test', 'drupal', 'drupal_custom'
+$settings['dfg_env'] = 'drupal_custom'; // possible values 'prod', 'test', 'drupal', 'drupal_custom'
+// For custom builds use drupal_custom; then clear cache (drush cr) to apply library switch.
